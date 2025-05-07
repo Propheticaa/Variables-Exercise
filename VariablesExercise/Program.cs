69aae0791e9c07c52ff0887056a11b782d6d964a
Prophetica <samin.manshif@gmail.com>
2025-05-07 14:51:04 -0400

    Initialized and outputted variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 5;
+            string b = "abc";
+            char c = 'a';
+            bool d = true;
+            double e = 3.14;
+            decimal f = 3.14159265358979323846m;
+            Console.WriteLine($"Int: {a}");
+            Console.WriteLine($"String: {b}");
+            Console.WriteLine($"Char: {c}");
+            Console.WriteLine($"Bool: {d}");
+            Console.WriteLine($"Double: {e}");
+            Console.WriteLine($"Decimal: {f}");
         }
     }
 }
